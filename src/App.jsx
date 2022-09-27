b202309543b0e0f479adfa3b25e74ef19da85176
Niru-Ninja <niru.webdev@gmail.com>
2022-09-27 18:19:01 -0300

    ...

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import IpPanel from "./components/IpPanel";
+
+function App() {
+  return (
+    <div className="App">
+      <IpPanel/>
+    </div>
+  )
+}
+
+export default App
