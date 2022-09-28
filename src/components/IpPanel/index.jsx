2a1215ade2dd2b522161c32fe11ec496713a12a1
Niru-Ninja <niru.webdev@gmail.com>
2022-09-28 00:13:58 -0300

    I believe the API errors are fixed now?

--- src/components/IpPanel/index.jsx
+++ src/components/IpPanel/index.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import "./styles.css";
 
 
-
 const IpPanel = () => {
     const [clientIP, setClientIP] = useState("");
     const [clientISP, setClientISP] = useState("");
@@ -11,8 +10,7 @@ const IpPanel = () => {
     const [clientLat, setClientLat] = useState("");
     const [clientLong, setClientLong] = useState("");
 
-    const ipApiURL = "https://api64.ipify.org/?format=json";
-    const ispApiURL = "http://ip-api.com/json/";
+    const apiURL = "https://isp-middle-api.nrueda687.repl.co/whoami";
 
     useEffect(() => {
         const getClientGeoLoc = () => {
@@ -23,42 +21,35 @@ const IpPanel = () => {
                 });
             }
         }
-        const getClientIP = async() => {
-            fetch(ipApiURL)
+        const getClientData = async() => {
+            fetch(apiURL)
             .then(response => response.json())
             .then(data => {
+                console.log(data);
                 setClientIP(data.ip);
-                console.log(data.ip)
-            });
-        };
-        const getClientISP = async(ipString) => {
-            fetch(ispApiURL + ipString)
-            .then(response => response.json())
-            .then(data => {
                 setClientCountry(data.country);
                 setClientRegion(data.city);
                 setClientISP(data.isp);
-                console.log(data.isp)
-            });
-        };
+            })
+        }
         getClientGeoLoc();
-        getClientIP().then(()=>{getClientISP(clientIP);});
+        getClientData();
     });
 
     return (
       <div id="contenedor" className="flexV">
-          <div id="data_localizacion">
-                {clientCountry.length === 0 ? <h2 id="pais">Cargando Coordenadas</h2> : <h2 id="pais">{clientCountry}</h2>}
-                {clientRegion.length === 0 ? <h3 id="region">...</h3> : <h3 id="region">{clientRegion}</h3>}
-          </div>
-          <div id="data_coordenadas" className="flexH">
-                {clientLat.length === 0 ? <h4 id="lat" className="flexItem">Latitud: </h4> : <h4 id="lat" className="flexItem">Latitud: {clientLat}</h4>}
-                {clientLong.length === 0 ? <h4 id="long" className="flexItem">Longitud: </h4> : <h4 id="long" className="flexItem">Longitud: {clientLong}</h4>}
-          </div>
-          <div id="data_internet">
-                {clientIP.length === 0 ? <h2 id="ip">IP: </h2> : <h2 id="ip">{clientIP}</h2>}
-                {clientISP === 0 ? <h2 id="isp">Proveedor: </h2> : <h2 id="isp">{clientISP}</h2>}
-          </div>
+        <div id="data_localizacion">
+              {clientCountry.length === 0 ? <h2 id="pais">Cargando Coordenadas</h2> : <h2 id="pais">{clientCountry}</h2>}
+              {clientRegion.length === 0 ? <h3 id="region">...</h3> : <h3 id="region">{clientRegion}</h3>}
+        </div>
+        <div id="data_coordenadas" className="flexH">
+              {clientLat.length === 0 ? <h4 id="lat" className="flexItem">Latitud: </h4> : <h4 id="lat" className="flexItem">Latitud: {clientLat}</h4>}
+              {clientLong.length === 0 ? <h4 id="long" className="flexItem">Longitud: </h4> : <h4 id="long" className="flexItem">Longitud: {clientLong}</h4>}
+        </div>
+        <div id="data_internet">
+              {clientIP.length === 0 ? <h2 id="ip">IP: </h2> : <h2 id="ip">{clientIP}</h2>}
+              {clientISP === 0 ? <h2 id="isp">Proveedor: </h2> : <h2 id="isp">{clientISP}</h2>}
+        </div>
       </div>
     )
 }
